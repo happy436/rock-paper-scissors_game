7df53a5d2fc4f170cd490557d23ce4d51d544ed9
admin <admin@Happy436.local>
2022-08-06 18:02:22 +0300

    prettier

--- react-client/.eslintrc.js
+++ react-client/.eslintrc.js
@@ -1,22 +1,32 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    env: {
+        browser: true,
+        es2021: true
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
-}
+    extends: ["plugin:react/recommended", "standard"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    plugins: ["react"],
+    rules: {
+        "multiline-ternary": ["off"],
+        indent: ["error", 4, { SwitchCase: 1 }],
+        semi: [2, "always"],
+        "space-before-function-paren": [
+            "error",
+            { anonymous: "always", named: "never" }
+        ],
+        quotes: [
+            "error",
+            "double",
+            {
+                allowTemplateLiterals: true,
+                avoidEscape: true
+            }
+        ]
+    }
+};
