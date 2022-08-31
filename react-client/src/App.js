ef50e5976441ec43ccf3c18648a989aa253c54df
Olev Vildyakskin <lelouch436@gmail.com>
2022-08-31 18:05:30 +0300

    add container component

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -7,11 +7,11 @@ import LogOut from "./layouts/logOut";
 import Login from "./layouts/login";
 import Game from "./components/pages/game";
 import Rating from "./components/pages/rating";
-import Profile from "./components/pages/profile";
 import Header from "./components/header";
 /* import ProtectedRoute from "./components/common/protectedRoute"; */
 import Settings from "./components/pages/settings/settings";
-import Achievements from "./components/pages/achievements/achievements";
+import ProfileWrapper from "./components/pages/profile/profileWrapper";
+import AchievementsContainer from "./components/pages/achievements/achievementsContainer";
 
 function App() {
     const isLoggedIn = false;
@@ -35,13 +35,13 @@ function App() {
                     <Route path="/game/:type?" component={Game} />
                     <Route
                         path="/achievements/:userId?"
-                        component={Achievements}
+                        component={AchievementsContainer}
                     />
-                    <Route path="/profile/:userId?" component={Profile} />
+                    <Route path="/profile/:userId?" component={ProfileWrapper} />
                     <Route path="/settings" component={Settings} />
                     <Route path="/login/:type?" component={Login} />
                     <Route path="/logout" component={LogOut} />
-                    <Route path="/" exact component={Main} />
+                    <Route path={isLoggedIn ? "/home" : "/"} exact component={Main} />
                     <Redirect to={isLoggedIn ? "/home" : "/"} />
                 </Switch>
             </main>
