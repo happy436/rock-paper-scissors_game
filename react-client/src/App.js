7e538a5a7a953c038d19672a3881aeadf72ae5a0
happy436 <lelouch436@gmail.com>
2022-09-08 19:50:12 +0300

    animation

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import React /* , { useEffect, useState } */ from "react";
+import { Route, Switch, Redirect, useLocation } from "react-router-dom";
+import { useTransition, animated } from "react-spring";
 import { ToastContainer } from "react-toastify";
 import AppLoader from "./components/hoc/appLoader";
 import Main from "./components/pages/main";
@@ -12,45 +13,64 @@ import Header from "./components/header";
 import Settings from "./components/pages/settings/settings";
 import AchievementsContainer from "./components/pages/achievements/achievementsContainer";
 import ProfileContainer from "./components/pages/profile/profileContainer";
-import history from "./utils/history";
 
 function App() {
     const isLoggedIn = false;
-    const urlWithoutHeader = ["/login/login", "/login/register", "/", "/game", ""];
-    const [urlHistory, setUrlHistory] = useState(history.location.pathname);
-    const [showHeader, setShowHeader] = useState(false);
+    const location = useLocation();
+    const transition = useTransition(location, {
+        from: {
+            opacity: 0
+        },
+        enter: {
+            opacity: 1
+        },
+        leave: {
+            opacity: 0
+        }
+    });
+    /* const urlWithoutHeader = ["/login/login", "/login/register", "/", "/game", ""];
+    const [urlHistory, setUrlHistory] = useState(location.pathname);
+    const [showHeader, setShowHeader] = useState(true);
     useEffect(() => {
-        setUrlHistory(history.location.pathname);
+        setUrlHistory(location.pathname);
         setShowHeader(!urlWithoutHeader.includes(urlHistory));
-    }, [history.location.pathname]);
+    }, [location.pathname]); */
     return (
         <AppLoader>
             <h1 hidden>Rock, paper, scissors</h1>
-            {showHeader && <Header />}
-            <main className="flex justify-center w-full">
-                <Switch>
-                    {/*
+            {/* {showHeader &&  */}
+            <Header />
+            {/* } */}
+            <main className="flex justify-center relative">
+                {transition((props, item) => (
+                    <animated.div style={props}>
+                        <div className="absolute w-full left-0">
+                            <Switch location={item}>
+                                {/*
                 <ProtectedRoute path="/game" component={Game} /> */}
-                    <Route path="/rating" component={Rating} />
-                    <Route path="/game/:type?" component={Game} />
-                    <Route
-                        path="/achievements/:userId?"
-                        component={AchievementsContainer}
-                    />
-                    <Route
-                        path="/profile/:userId?"
-                        component={ProfileContainer}
-                    />
-                    <Route path="/settings" component={Settings} />
-                    <Route path="/login/:type?" component={Login} />
-                    <Route path="/logout" component={LogOut} />
-                    <Route
-                        path={isLoggedIn ? "/home" : "/"}
-                        exact
-                        component={Main}
-                    />
-                    <Redirect to={isLoggedIn ? "/home" : "/"} />
-                </Switch>
+                                <Route path="/rating" component={Rating} />
+                                <Route path="/game/:type?" component={Game} />
+                                <Route
+                                    path="/achievements/:userId?"
+                                    component={AchievementsContainer}
+                                />
+                                <Route
+                                    path="/profile/:userId?"
+                                    component={ProfileContainer}
+                                />
+                                <Route path="/settings" component={Settings} />
+                                <Route path="/login/:type?" component={Login} />
+                                <Route path="/logout" component={LogOut} />
+                                <Route
+                                    path={isLoggedIn ? "/home" : "/"}
+                                    exact
+                                    component={Main}
+                                />
+                                <Redirect to={isLoggedIn ? "/home" : "/"} />
+                            </Switch>
+                        </div>
+                    </animated.div>
+                ))}
             </main>
             <ToastContainer />
         </AppLoader>
