a4e529e1aec591b348923f7c22745f0c59d01c45
happy436 <lelouch436@gmail.com>
2022-09-23 17:50:27 +0300

    data

--- react-client/src/store/gameData.js
+++ react-client/src/store/gameData.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import gameDataService from "../services/gameData.service";
 
-/* const initState = {
+const initState = {
     achievements: [
         {
             title: "I am the best",
@@ -109,16 +109,12 @@ import gameDataService from "../services/gameData.service";
             spock: "draw"
         }
     }
-}; */
+};
 
 const gameDataSlice = createSlice({
     name: "gameData",
     initialState: {
-<<<<<<< HEAD
         entities: initState,
-=======
-        entities: null,
->>>>>>> e8cac8c3bf1d3955e0648e48fb4c971b020b234a
         isLoading: true,
         error: null
     },
